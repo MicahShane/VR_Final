b2ed98af89cb84fec82cab1e35e8d05099a6d661
Micah Taylor <micah.shane.85@gmail.com>
2018-02-26 23:50:06 -0500

    summary
    networking!!
    
    Co-Authored-By: mat704 <mat704@users.noreply.github.com>

---
+++ VRClass_Group4/Assets/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject vrCameraRig;
+	public GameObject nonVRCameraRig;
+	public Blinker hmdBlinker;
+	public Transform SteamVR_Rig;
+	public SteamVR_TrackedObject hmd;
+	public SteamVR_TrackedObject controllerLeft;
+	public SteamVR_TrackedObject controllerRight;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void enableVR(){
+		StartCoroutine(doEnableVR());
+	}
+
+	IEnumerator doEnableVR(){
+		while(XRSettings.loadedDeviceName != "OpenVR"){
+			XRSettings.LoadDeviceByName("OpenVR");
+			yield return null;
+		}
+		XRSettings.enabled = true;
+		vrCameraRig.SetActive(true);
+		nonVRCameraRig.SetActive(false);
+	}
+}
