9870dd82b6f70cacfa9018f18f7a498de2158dcf
Charlie <ced89538@uga.edu>
2018-03-04 00:41:10 -0500

    interactions
    started work on interaction
    continued maintence on locomotion
    
    Co-Authored-By: Micah Taylor <micahshane@users.noreply.github.com>

---
+++ Interactions/Assets/floodSim.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floodSim : MonoBehaviour {
+    public GameObject floodWater;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1")) //change to cube collider 
+        {
+            floodWater.transform.Translate(Vector3.up * 1.2f);
+
+        }
+	}
+}
