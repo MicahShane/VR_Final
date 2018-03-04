b0fa9a9aff6e4e5c55173fa13bd6d880b4d9ad85
Micah Taylor <micah.shane.85@gmail.com>
2018-03-04 18:55:35 -0500

    interaction scripts
    testing in sandbox because network version cannot access hands

--- Interactions/Assets/narrativePlayer.cs
+++ Interactions/Assets/narrativePlayer.cs
@@ -3,14 +3,78 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class narrativePlayer : MonoBehaviour {
+	public AudioSource narrative;
+	public GameObject leftHand;
+	public GameObject rightHand;
+	public string lhName;
+	public string rhName;
+	public float playTime;
+	public float initTime;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start()
+	{
+		lhName = leftHand.name;
+		rhName = rightHand.name;
+		narrative = GetComponent<AudioSource>();
+		initTime = playTime;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+		/* if (Input.GetButtonDown("Fire1")) //change to cube collider 
+		 {
+			 floodWater.transform.Translate(Vector3.up * 1.2f);
+
+		 }*/
+
 	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.name.Equals(lhName) || col.gameObject.name.Equals(rhName))
+		{
+			if (narrative.isPlaying)
+			{
+				narrative.Pause();
+				Debug.Log("paused");
+			}
+			else
+			{
+				Debug.Log("Its playing!!");
+				narrative.Play();
+				StartCoroutine(clipTimer(playTime));
+				
+			}
+		}
+	}
+	float currTime;
+	bool isPaused = false;
+	public IEnumerator clipTimer(float countdownValue)
+	{
+		currTime = countdownValue;
+		while (currTime > 0 )
+		{
+			Debug.Log("Countdown: " + currTime);
+			yield return new WaitForSeconds(1.0f);
+			currTime--;
+			if (!(narrative.isPlaying)&& narrative.time != 0)
+			{
+				playTime = currTime;
+				isPaused = true;
+				break;
+			}
+		}
+		
+		if (isPaused == false)
+		{
+			playTime = initTime;
+			narrative.Stop();
+		}
+		else
+		{
+			isPaused = false;
+		}
+		}
 }
--- Interactions/Assets/shrimping.cs
+++ Interactions/Assets/shrimping.cs
@@ -4,13 +4,55 @@ using UnityEngine;
 
 public class shrimping : MonoBehaviour {
 
+	public GameObject ShrimpBoat_prefab;
+	public GameObject leftHand;
+	public GameObject rightHand;
+	public string lhName;
+	public string rhName;
+	public GameObject sb;
+	//public Renderer rend;
+	private bool hideshow = false;
+	//public Transform[] parents;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start()
+	{
+		//parents = ShrimpBoat.GetComponentsInChildren<Transform>();
+		lhName = leftHand.name;
+		rhName = rightHand.name;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.name.Equals(lhName) || col.gameObject.name.Equals(rhName))
+		{
+			
+			//make shrimp boats appear and disappear
+			hideshow = !hideshow;
+			Debug.Log("Hideshow is: " + hideshow);
+
+			if (hideshow)
+			{
+
+				//make shrimp boat appear
+				sb = Instantiate(ShrimpBoat_prefab);
+				
+			}
+			else
+			{
+
+
+				//make shrimp boat disappear
+				Destroy(sb);
+				
+			}
+
+
+		}
 	}
 }
