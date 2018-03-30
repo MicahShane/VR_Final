02ee7f4f560fd840061c7ca267d4c49f0856f7b4
mat704 <32942209+mat704@users.noreply.github.com>
2018-03-30 19:48:33 -0400

    putting some text on a sign
    me + Micah

---
+++ group4DarienTour/group4DarienTour/Assets/Blinker/Blinker.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blinker : MonoBehaviour {
+
+	public Material myMat;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	private void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		Graphics.Blit(source, destination, myMat);
+	}
+	public void blink(float t)
+	{
+		StartCoroutine(doBlink(t));
+	}
+	IEnumerator doBlink(float t)
+	{
+		float tt = 0;
+		while (tt < t)
+		{
+			Debug.Log(tt);
+			tt = tt + Time.deltaTime;
+			if (tt >= t)
+			{
+				tt = t;
+			}
+			myMat.SetColor("_BlinkColor", Color.Lerp(Color.white, Color.black, tt / t));
+			yield return null;
+		}
+		tt = 0;
+		while (tt < t)
+		{
+
+			tt = tt + Time.deltaTime;
+			if (tt >= t)
+			{
+				tt = t;
+			}
+			myMat.SetColor("_BlinkColor", Color.Lerp(Color.black, Color.white, tt / t));
+			yield return null;
+		}
+	}
+}
---
+++ group4DarienTour/group4DarienTour/Assets/shorelineVisual.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shorelineVisual : MonoBehaviour {
+
+	public GameObject shore1800_Prefab;
+	public GameObject shore1930_Prefab;
+	public GameObject shore2000_Prefab;
+
+	public GameObject leftHand;
+	public GameObject rightHand;
+	public string lhName;
+	public string rhName;
+
+	
+	public GameObject dlog;
+	public dataLog dscript;
+	public GameObject sl;
+
+	private int counter = 0;
+
+	void Start()
+	{
+		
+		lhName = leftHand.name;
+		rhName = rightHand.name;
+		dscript = dlog.GetComponent<dataLog>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.name.Equals(lhName) || col.gameObject.name.Equals(rhName))
+		{
+			dscript.logEvent(this.name);
+			
+
+			Debug.Log("counter is: " + counter);
+
+			if (counter == 0)
+			{
+				
+				sl = Instantiate(shore1800_Prefab);
+				counter++;
+			}
+			else if (counter == 1)
+			{
+				
+				Destroy(sl);
+				sl = Instantiate(shore1930_Prefab);
+				counter++;
+
+			} else if (counter == 2){
+				Destroy(sl);
+				sl = Instantiate(shore2000_Prefab);
+				counter++;
+
+			}else{
+				Destroy (sl);
+				counter = 0;
+	}
+		}
+
+}
+}
