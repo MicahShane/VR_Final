8cb858ab0ea873cf1041e11a458c3735f2e41f9e
Ryan Cruz <rac71636@uga.edu>
2018-04-17 01:01:18 -0400

    add years
    add years  when touching the shoreline_sign
    
    Co-Authored-By: Micah Taylor <micahshane@users.noreply.github.com>
    Co-Authored-By: zhenyu yan <469022042@qq.com>
    Co-Authored-By: dj323 <dj323@users.noreply.github.com>
    Co-Authored-By: Ryan Cruz <racruz7@me.com>

--- group4DarienTour/Assets/shorelineVisual.cs
+++ group4DarienTour/Assets/shorelineVisual.cs
@@ -7,8 +7,12 @@ public class shorelineVisual : MonoBehaviour {
 	public GameObject shore1800_Prefab;
 	public GameObject shore1930_Prefab;
 	public GameObject shore2000_Prefab;
+    public GameObject shore1800_Text;
+    public GameObject shore1930_Text;
+    public GameObject shore2000_Text;
 
-	public GameObject leftHand;
+
+    public GameObject leftHand;
 	public GameObject rightHand;
 	public string lhName;
 	public string rhName;
@@ -17,8 +21,9 @@ public class shorelineVisual : MonoBehaviour {
 	public GameObject dlog;
 	public dataLog dscript;
 	public GameObject sl;
+    public GameObject st;
 
-	private int counter = 0;
+    private int counter;
 
 	void Start()
 	{
@@ -26,6 +31,8 @@ public class shorelineVisual : MonoBehaviour {
 		lhName = leftHand.name;
 		rhName = rightHand.name;
 		dscript = dlog.GetComponent<dataLog>();
+        counter = 0;
+         
 	}
 
 	// Update is called once per frame
@@ -46,7 +53,7 @@ public class shorelineVisual : MonoBehaviour {
 
             if (counter == 0)
             {
-
+                st = Instantiate(shore1800_Text);
                 sl = Instantiate(shore1800_Prefab);
                 counter++;
             }
@@ -54,19 +61,27 @@ public class shorelineVisual : MonoBehaviour {
             {
 
                 Destroy(sl);
+                Destroy(st);
+                st = Instantiate(shore1930_Text);
                 sl = Instantiate(shore1930_Prefab);
+              
                 counter++;
 
             }
             else if (counter == 2)
             {
+                ;
                 Destroy(sl);
+                Destroy(st);
+                st = Instantiate(shore2000_Text);
                 sl = Instantiate(shore2000_Prefab);
+              
                 counter++;
 
             }
             else
             {
+                Destroy(st);
                 Destroy(sl);
                 counter = 0;
             }
