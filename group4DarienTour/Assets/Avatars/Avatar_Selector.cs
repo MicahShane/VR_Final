37f8d4a92843810fdc240e5a9a815bb89b12f53a
Micah Taylor <micah.shane.85@gmail.com>
2018-04-24 18:34:20 -0400

    Avatar Selection Menu
    Added an avatar selection menu when the game starts.  The avatars are head scans of the group 1 participants

---
+++ group4DarienTour/Assets/Avatars/Avatar_Selector.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Avatar_Selector : MonoBehaviour {
+
+    public GameObject Avatar_Selector_UI;
+    public GameObject Associated_Avatar;
+
+   
+    public GameObject rightHand;
+    public string rhName;
+
+
+    // Use this for initialization
+    void Start () {
+
+        rhName = rightHand.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter(Collider col)
+      {
+
+         if (col.gameObject.name.Equals(rhName))
+            {
+            Associated_Avatar.SetActive(true);
+            Avatar_Selector_UI.SetActive(false);
+            }
+        }
+    }
+
+
+    
+
